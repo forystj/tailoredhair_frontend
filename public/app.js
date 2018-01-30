3ca7d797fb2c4a0c05dcc3afbbcf6dea95ea6477
Foryst Jankelat commit --amend --reset-author <fjankela@outlook.com>
2018-01-30 14:12:05 -0800

    fixing profile deploy

--- public/app.js
+++ public/app.js
@@ -137,10 +137,10 @@ this.url = 'https://tailoredhair-api.herokuapp.com';
     console.log(response.data);
     this.allLooks = response.data;
     // console.log(this.allLooks[0].user.id);
-    // console.log(this.currentUser);
+    console.log(this.currentUser);
     for (i=0; i<this.allLooks.length; i++){
       console.log(this.allLooks[i].user_id);
-      console.log(this.currentUser[i]);
+      console.log(this.currentUser);
       // if (this.allLooks[i].user_id = this.currentUser[0].id) {
       //   this.currentPosts.push(this.allLooks[i])
       //   console.log(this.currentPosts);
